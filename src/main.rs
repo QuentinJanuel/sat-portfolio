2481d6dca84a2b3c11a46e8561ab5fca432486ea
DapperMink <quentinjanuelkij@gmail.com>
2022-02-14 23:22:58 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 extern crate sat_portfolio;
 
 mod minisat {
-    #![allow(unused)]
+    #![allow(
+        unused,
+        non_upper_case_globals,
+        non_camel_case_types,
+        non_snake_case,
+    )]
     include!(concat!(env!("OUT_DIR"), "/minisat-bindings.rs"));
 }
 
